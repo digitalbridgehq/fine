df9f807aae706c0bcd9491aa009c76111b54d5bb
Jay Faulkner <jayleefaulkner@gmail.com>
2019-02-13 15:35:47 +0000

    Add: stryker conf and run script
    `yarn test:mutation`

---
+++ stryker.conf.js
@@ -0,0 +1,15 @@
+module.exports = function(config) {
+  config.set({
+    mutator: "typescript",
+    packageManager: "yarn",
+    reporters: ["clear-text", "progress"],
+    testRunner: "jest",
+    transpilers: [],
+    coverageAnalysis: "off",
+    tsconfigFile: "tsconfig.json",
+    mutate: [
+        "src/**/*.ts",
+        "!src/**/*.test.ts"
+    ]
+  });
+};
